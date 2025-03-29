101a606993eddbe5079d4d6c899840f4ba8542cb
Adi Purnama <adiprnm2014@gmail.com>
2025-03-29 07:55:19 +0700

    Fetch directly

--- public/js/script.js
+++ public/js/script.js
@@ -8,9 +8,5 @@
     const ref = [refParam, document.referrer].find(r => r && getDomain(r) !== currentDomain) || '';
     const baseUrl = script.src.split('/script.js')[0]
     const url =`${baseUrl}/hit?path=${encodeURIComponent(window.location.pathname)}&ref=${encodeURIComponent(ref)}&website=${site}&page=${page}`
-
-    document.addEventListener('DOMContentLoaded', () => {
-      document.querySelector('body').addEventListener('mouseover', () => fetch(url), { once: true });
-      document.addEvenetListener('touchstart', () => fetch(url), { once: true });
-    })
+    fetch(url)
 })();
